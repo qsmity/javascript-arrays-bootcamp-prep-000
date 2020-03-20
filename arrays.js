f93a76736ca5e4030454a3ea9205a8caca465b47
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-20 08:56:38 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 =  "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ['snicker', "hundred grand", "kitkat", 'skittles']
+
+var addElementToBeginningOfArray = function (array,element){
+ const array2 = array.unshift(element)
+ return array 
+}
+
+var destructivelyAddElementToBeginningOfArray = function (array, element){
+  array.unshift(element)
+  return array
+}
+
+
+var addElementToEndOfArray = function (array,element){
+ const array2 = array.push(element)
+ return array 
+}
+
+var destructivelyAddElementToEndOfArray = function (array, element){
+  array.push(element)
+  return array
+}
+
+var accessElementInArrayj = function(array, index){
+  return array[index]
+}
+
+// console.log(addElementToBeginningOfArray(chocolateBars, "payday"));
+
+// console.log(destructivelyAddElementToBeginningOfArray(chocolateBars, "cars"));
+
+// console.log(addElementToEndOfArray(chocolateBars,"heathbar"))
+
+// console.log(destructivelyAddElementToEndOfArray(chocolateBars,"recesecup"))
